2dcd7448b896695f0a8479067562b279be4a9b4c
kelvin <kelvin.xu@ubc.ca>
2022-12-12 15:06:44 -0800

    add block deprecation so that existing blocks will use the supported start and end datetime attribute

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'd4616c419cfae1ca46b8');
+<?php return array('dependencies' => array('wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'bb943dfd12e09d71ccc9');
